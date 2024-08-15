96bb124a56c228ee85e0ed48e9040301d7b5883a
Jonathan Weiß <jonathan.weiss.1@student.hu-berlin.de>
2024-08-15 11:45:15 +0000

    strange state where the difference image shows desired result

--- mreTools/kthpinn/misc/loss.py
+++ mreTools/kthpinn/misc/loss.py
@@ -49,14 +49,32 @@ def complex_mse_loss(y_true, y_pred, batch_dim=None):
     dims_to_reduce.remove(batch_dim)
     return torch.mean(abs, dim=dims_to_reduce)
 
-def normalized_complex_mse_loss(y_true, y_pred):
+def normalized_complex_mse_loss(y_true, y_pred, batch_dim=None):
     '''Computes the mse loss seperately for the first and second item of the first component. Then normalizes both results with the value range of the true image and returns the sum of both.
     Expects input of shape (batch_size, 2, height, width, ...)'''
-    mse = torch.nn.MSELoss()
-    real_loss = mse(y_true[:,0], y_pred[:,0])
-    imag_loss = mse(y_true[:,1], y_pred[:,1])
-    print(real_loss, imag_loss)
-    return real_loss / torch.max(y_true[:,0]) + imag_loss / torch.max(y_true[:,1])
+    real_loss_no_mean = (y_true[...,0] - y_pred[...,0])**2
+    imag_loss_no_mean = (y_true[...,1] - y_pred[...,1])**2
+    real_loss = 0
+    imag_loss = 0
+
+    shape = y_true[..., 0].shape
+    num_of_dims = len(shape)
+    
+    if batch_dim is None:
+        real_loss = torch.mean(real_loss_no_mean)
+        imag_loss = torch.mean(imag_loss_no_mean)
+
+    if num_of_dims == 1:    # there is a batch dim given but only one dimension exists. In this case, we do not take the mean
+        real_loss = real_loss_no_mean
+        imag_loss = imag_loss_no_mean
+    else:
+        dims_to_reduce = list(range(num_of_dims))
+        dims_to_reduce.remove(batch_dim)
+
+        real_loss = torch.mean(real_loss_no_mean, dim=dims_to_reduce)
+        imag_loss = torch.mean(imag_loss_no_mean, dim=dims_to_reduce)
+    
+    return real_loss / torch.max(y_true[...,0]) + imag_loss / torch.max(y_true[...,1])
 
 def normalized_complex_mae_loss(y_true, y_pred):
     '''Computes the mae loss seperately for the first and second item of the first component. Then normalizes both results with the value range of the true image and returns the sum of both.
